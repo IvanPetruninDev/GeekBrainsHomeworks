c6077817cc609a9659eb21986567d935c2cef791
IvanPetruninDev <ivanpetrunin_arch@icloud.com>
2021-03-18 23:46:10 +0500

    Подправил везде названия и доотправил все три работы

--- homeworks_day_one/Homework 1 ex. 2.py
+++ homeworks_day_one/Homework 1 ex. 2.py
@@ -1,36 +1,32 @@
 cube_list = []
 
-for number in range (1, 1000):
+for number in range (1, 1000, 2):
     cube = number ** 3
     cube_list.append(cube)
 
 result_sum = 0
-for i in cube_list:
+for element in cube_list:
     num_sum = 0
-    test_element = 1
-    # ищем сумму цифр числа
+    test_element = element
     while test_element != 0:
-        num_sum = num_sum + test_element % 10
+        num_sum += test_element % 10
         test_element = test_element // 10
-    # проверка условия
+
     if num_sum % 7 == 0:
         result_sum += num_sum
 
 result_sum_changed = 0
-for index, elem in enumerate(cube_list):
+for elem in cube_list:
     test_element = elem + 17
-    cube_list[index] = test_element
     num_sum = 0
-    # ищем сумму цифр числа
     while test_element != 0:
-        num_sum = num_sum + test_element % 10
+        num_sum += test_element % 10
         test_element = test_element // 10
-    # проверка условия
-    if num_sum % 7  == 0:
+    if num_sum % 7 == 0:
         result_sum_changed += num_sum
 
-    print('Result of first list:' + (str(result_sum)))
-    print('Result of changed list:' + (str(result_sum_changed)))
+print('Result of first list:' + (str(result_sum)))
+print('Result of changed list:' + (str(result_sum_changed)))
 
 
 
